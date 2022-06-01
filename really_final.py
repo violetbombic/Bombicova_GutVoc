24072ee8742c0aaa4108e9476547e4018067f7a4
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-06-01 10:40:40 +0200

    Update really_final.py

--- really_final.py
+++ really_final.py
@@ -1,3 +1,4 @@
+#IMPORTS AND DOWNLOADS
 import streamlit as st
 import PyPDF2
 from PyPDF2 import PdfFileReader
@@ -7,26 +8,27 @@ nltk.download('punkt')
 import io
 from io import StringIO
 
-
+#TITLE AND DESCRIPTION
 st.title("Project_Name")
 st.write("Please insert a relative path of the PDF file. Note that the PDF file has to be saved in and copied from the same folder in GitHub, where  the .py file is. Try for example: CORE_INTENTIONAL_FEATURES_IN_THE_SYNTACT.pdf")
 
-relative_path = st.file_uploader("Please choose a file")
 
-if relative_path is not None:  
+#INPUT
+option = st.radio("What type of file you want to upload?", ('Text file', 'url'))
+if option == 'Text file':
+    updated_file = st.file_uploader("Please choose a file")
+if option == 'url':
+    st.text_input("Please insert an url")
 
-    # creating a pdf reader object 
-    pdfReader = PdfFileReader(relative_path) 
+# if updated_file is not None:  
+#     pdfReader = PdfFileReader(uploaded_file) 
+#     num_pages = pdfReader.numPages
 
-    # printing number of pages in pdf file 
-    num_pages = pdfReader.numPages
-
-    page_content=""
-    number_of_pages = pdfReader.getNumPages()
-
-    for page_number in range(number_of_pages):
-        page = pdfReader.getPage(page_number)
-        page_content += page.extract_text()
-    st.write(page_content)
+#     page_content=""
+#     number_of_pages = pdfReader.getNumPages()
+#     for page_number in range(number_of_pages):
+#         page = pdfReader.getPage(page_number)
+#         page_content += page.extract_text()
+#     st.write(page_content)
 
 
