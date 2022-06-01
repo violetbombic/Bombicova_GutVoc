960369446c272913daf08bbc9a1a707b49bebd03
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-06-01 10:56:13 +0200

    Update really_final.py

--- really_final.py
+++ really_final.py
@@ -17,16 +17,13 @@ st.write("Please insert a relative path of the PDF file. Note that the PDF file 
 option = st.selectbox("What type of file you want to upload?", ('Text file', 'url'))
 if option == 'Text file':
     uploaded_file = st.file_uploader("Please choose a file")
-    if uploaded_file is not None:  
-        pdfReader = PdfFileReader(uploaded_file) 
-        num_pages = pdfReader.numPages
+    if uploaded_file is not None:
+        stringio = StringIO(uploaded_text_file.getvalue().decode("utf-8"))
+        string_data = stringio.read()
+        text = string_data
+        st.subheader("Here is your text: ")
+        st.text_area(text)
 
-        page_content=""
-        number_of_pages = pdfReader.getNumPages()
-        for page_number in range(number_of_pages):
-            page = pdfReader.getPage(page_number)
-            page_content += page.extract_text()
-        st.write(page_content)
     
 
 elif option == 'url':
