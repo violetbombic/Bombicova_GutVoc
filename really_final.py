908bdc5d94b5639be4646e3c29a7d9680a39403c
violetbombic <101185749+violetbombic@users.noreply.github.com>
2022-06-03 11:13:51 +0200

    Update really_final.py

--- really_final.py
+++ really_final.py
@@ -81,6 +81,7 @@ final_list = [word for word in no_double_list if len(word) >= 3]
 st.write(final_list)
 
 # Words translation
+lang = st.text_input('Please choose a language for translation: (for example en, sk, it, de, ur...) ')
 translator = Translator()
 translation = []
 for word in final_list:
